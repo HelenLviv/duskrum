06992507dd7c0921917203e0096883b5cc0242b1
HelenLviv <helark0@rambler.ru>
2016-07-02 08:57:36 +0000

    duscrum

---
+++ index.js
@@ -0,0 +1,19 @@
+function duskruminant(a,b,c) {
+  return sup2(b)-(4*a*c);
+  function x(a,b,duskruminant) {
+  if (duskruminant>0) {
+  return ((-b)+radic(duskruminant))/(2*a);
+  return ((-b)-radic(duskruminant))/(2*a);
+  }
+  if (duskruminant=0) {
+      return ((-b)+radic(d))/(2*a);
+  }
+      else {
+          retutn "no answer";
+      }
+  }
+}
+var a=1;
+var b=2;
+var c=3;
+alert (result)
